f2a0e2beda3de4c7583be013e95a8efa2569e859
dimka_228 <340384@niuitmo.ru>
2024-07-29 06:05:27 +0300

    проверка  на мобильные устройства

--- js/index.js
+++ js/index.js
@@ -2,20 +2,21 @@ $(() => {
 
     //scrollbar
     // $('.custom-scroll').scrollbar();
+    if(!isMobile()){
+        console.log('js loaded');
 
-    console.log('js loaded');
+        $view = $('.view');
+        $view.css('position','fixed');
+        $view.css('top','0');
+        $view.css('left','0');
+        $view.css('right','0');
+        $view.css('bottom','0');
 
-    $view = $('.view');
-    $view.css('position','fixed');
-    $view.css('top','0');
-    $view.css('left','0');
-    $view.css('right','0');
-    $view.css('bottom','0');
+        var Scrollbar = window.Scrollbar;
 
-    var Scrollbar = window.Scrollbar;
-
-    Scrollbar.init($view[0], {
-        damping:0.14,
-    });
+        Scrollbar.init($view[0], {
+            damping:0.14,
+        });
+    }
 
 });
