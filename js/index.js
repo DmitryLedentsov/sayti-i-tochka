5b1190e79f255c4dd755000e5dd8080e9a45d38c
dimka_228 <340384@niuitmo.ru>
2024-07-29 05:48:43 +0300

    подключил типа скроллбар

--- js/index.js
+++ js/index.js
@@ -3,4 +3,19 @@ $(() => {
     //scrollbar
     // $('.custom-scroll').scrollbar();
 
+    console.log('js loaded');
+
+    $view = $('.view');
+    $view.css('position','fixed');
+    $view.css('top','0');
+    $view.css('left','0');
+    $view.css('right','0');
+    $view.css('bottom','0');
+
+    var Scrollbar = window.Scrollbar;
+
+    Scrollbar.init($view[0], {
+        damping:0.14,
+    });
+
 });
