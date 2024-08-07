cc9545cb32fe6787ab20c47205161136f8b47848
dimka_228 <340384@niuitmo.ru>
2024-08-07 05:45:40 +0300

    загодовка под жс

--- js/index.js
+++ js/index.js
@@ -1,22 +1,35 @@
 $(() => {
-
-    //scrollbar
-    // $('.custom-scroll').scrollbar();
+    console.log('js loaded ', `mobile mode: ${isMobile()}` );
+    //scrollbar только для больших мониторов
+    
     if(!isMobile()){
-        console.log('js loaded');
-
+ 
+        var scrollbar = window.Scrollbar;
         $view = $('.view');
         $view.css('position','fixed');
         $view.css('top','0');
         $view.css('left','0');
         $view.css('right','0');
         $view.css('bottom','0');
-
-        var Scrollbar = window.Scrollbar;
-
-        Scrollbar.init($view[0], {
-            damping:0.14,
+        
+        scrollbar = scrollbar.init($view[0], {
+            damping: 0.14,
         });
     }
 
+    //$('#btn).on('click', function(e){
+        scrollTo('#footer');// тут надо id расставить у всех элементов для скрола
+    //})
+  
+    //кроссбраузерная функция скрола
+    function scrollTo(id){
+        if(scrollbar) scrollbar.scrollIntoView(document.querySelector(id));
+        else{
+            $('html, body').animate({
+                scrollTop: $(id).offset().top
+            }, 2000);
+        }
+    }
+
 });
+
